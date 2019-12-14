497a145d18a41b766b66605a86526539717efa12
Viktor Alex <viktoralex@vodafone.is>
2019-12-14 19:59:47 +0000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var $form = $('form#rsvp-form'),
+    url = 'https://script.google.com/macros/s/AKfycbxiI-U2QqCe6rBEVdsHnGU7nVHLxg8_F4OrQcS1oFsXJk7Okl4/exec'
+
+$form.on('submit', function(e) {
+  e.preventDefault();
+
+  var data = {
+    nafn: $form.find('input[name=nafn]').val(),
+    maki: $form.find('input[name=maki]').val(),
+    oskir: $form.find('input[name=oskir]').val(),
+    netfang: $form.find('input[name=netfang]').val(),
+  };
+  console.log(JSON.stringify(data));
+
+  var jqxhr = $.ajax({
+    url: url,
+    method: "get",
+    //dataType: "json",
+    data: $form.serialize(),
+    //data: JSON.stringify(data),
+  }).success(
+    // do something
+  );
+})
